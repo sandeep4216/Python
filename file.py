9e64e91c82076d544e1dbbfa5101d33a9eadabe8
sandeep4216 <sandeepsoni4216@gmail.com>
2015-06-16 09:17:32 +0530

    sunny

--- file.py
+++ file.py
@@ -1,1 +1,16 @@
-no_of_employees = int(input("No of Employees? "))
+# # no_of_employees = int(input("No of Employees? "))
+
+# # for i in range(no_of_employees):
+# # 	print("Employee ",i)
+# # 	print("Employee nooo ",no_of_employees)
+
+
+# if expression :
+# 	suite
+# elif expression :
+# 	suite 
+# else :
+# 	suite
+import sys
+print ('Number of arguments:', len(sys.argv), 'arguments.')
+print ('Argument List:', str(sys.argv))
